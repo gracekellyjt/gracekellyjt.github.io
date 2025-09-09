df666fc3dec66ad16fb095bbb51a3a9f31ba5c03
gracekellyjt <traverg@email.sc.edu>
2025-09-09 15:04:35 -0400

    donecss

---
+++ csce242/examples/04_js/js/intro.js
@@ -0,0 +1,10 @@
+/*
+const sayHello = () => {
+    console.log("Hello World");
+}
+document.getElementById("btn-click-me").onclick = sayHello;
+*/
+
+document.getElementById("btn-click-me").onclick = () => {
+    console.log("hello world");
+}; 
