fd431fd5066f6aaea7780f5b3022bbdf4c68a356
gracekellyjt <traverg@email.sc.edu>
2025-09-15 23:32:14 -0400

    project html

--- csce242/examples/04_js/js/intro.js
+++ csce242/examples/04_js/js/intro.js
@@ -5,6 +5,33 @@ const sayHello = () => {
 document.getElementById("btn-click-me").onclick = sayHello;
 */
 
-document.getElementById("btn-click-me").onclick = () => {
-    console.log("hello world");
-}; 
+document.getElementById("btn-click-me").onclick = (event) => {
+    document.getElementById("p-welcome").innerHTML = "Hello World";
+    event.currentTarget.classList.add("clicked"); //current target is the nutton that waws clicked
+};
+document.getElementById("btn-happy").onclick = () => {
+    const pFeeling = document.getElementById("p-feeling");
+    pFeeling.innerHTML = "YAY";
+    pFeeling.classList.add("happy");
+    pFeeling.classList.remove("sad");
+};
+document.getElementById("btn-sad").onclick = () => {
+    const pFeeling = document.getElementById("p-feeling");
+    pFeeling.innerHTML = "Nay";
+    pFeeling.classList.add("sad");
+    pFeeling.classList.remove("happy");
+};
+document.getElementById("btn-clear").onclick = () => {
+    const pFeeling = document.getElementById("p-feeling");
+    pFeeling.innerHTML = "";
+    pFeeling.classList.remove("sad");
+    pFeeling.classList.remove("happy");
+};
+document.getElementById("txt-emotion").onekeyup = (event) => {
+    const userInput = event.currentTarget.value;
+    document.getElementById("p-emotion").innerHTML = `You are feeling ${userInput}.`
+    document.getElementById("img-emotion").classList.remove("hidden");
+}
+
+        
+        
